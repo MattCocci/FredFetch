3b12575beb0afd0096cc62066376f77d3b33d8c3
Matthew Cocci <Matthew.Cocci@ny.frb.org>
2015-06-04 19:10:37 -0400

    Laying groundwork for git multiple

--- vint.m
+++ vint.m
@@ -4,32 +4,13 @@ function [vintdata] = vint(series, vint_date, varargin)
     warning('Early vintage date; data might not exist, error likely.')
   end
 
-  %% Grab the data
-  flds = {'info'; 'obs'};
-  for f = 1:length(flds)
-    data.(flds{f}) = ReadFredURL(MakeFredURL(flds{f}, 'series_id', series, 'realtime_start', vint_date, 'realtime_end', vint_date, varargin{:}));
+  % Dispatch the call to different function depending upon whether one
+  % or multiple series are specified
+  if ~iscell(series), series = {series}; end
+  if length(series) == 1
+    vintdata = vint_single(series{:}, vint_date, varargin{:});
+  else
   end
 
-  %% Check that it downloaded
-  if isfield(data.obs, 'url')
-    error(sprintf('Could not download data using link\n\n\t%s\n', data.obs.url))
-  end
-  if isfield(data.info, 'url')
-    error(sprintf('Could not retrieve series info using link\n\n\t%s\n', data.info.url))
-  end
-
-  %% Parse the data
-  vintdata.info  = data.info.seriess{:};
-  data.obs       = vertcat(data.obs.observations{:});
-  nobs           = length(data.obs);
-  vintdata.date  = nan(nobs,1);
-  vintdata.value = nan(nobs,1);
-  for t = 1:nobs
-    vintdata.date(t) = datenum(data.obs(t).date, 'yyyy-mm-dd');
-    val = str2num(data.obs(t).value);
-    if ~isempty(val)
-      vintdata.value(t) = val;
-    end
-  end
 
 end
