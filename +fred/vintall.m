82f80ec4ef37aa009378c6a62ce2615002473b3f
Matthew Cocci <Matthew.Cocci@ny.frb.org>
2015-06-22 14:47:08 -0400

    Safer vintall that takes into account realtime_end.
    In vintall.m, we used  to construct an Nobs x Nvint matrix of values,
    where Nvint was the realtime_starts. This obviously ignored
    realtime_end.
    
    We would simply fill up the data matrix by looping over observations
    (looping over rows) and filling the data from realtime start to realtime
    start (filling left to right, overwriting vintage dates) in the vintage
    data matrix.
    
    This was maybe problematic. In particular, if there are two realtime
    starts (say 01-Jan-2000 and 01-Jan-2010) for two separate values, 10 and
    20, we used to put a value of 10 from 01-Jan-2000 until 31-Dec-2009.
    Then 20 from there. That's maybe wrong if there's a realtime_end for
    value 10 of, say, 01-Jan-2005.
    
    This could happen if a data provider decides to drop old
    observations because maybe they were wrong for some reason. To be
    fair, I have no concrete evidence this has occurred (except some
    vague recollection of seeing something like this at some point).
    Regardless, the new method implemented now is super safe and
    guaranteed to be correct, while the old method was not.

--- +fred/vintall.m
+++ +fred/vintall.m
@@ -6,7 +6,7 @@ function [vintdata] = vintall(series, varargin)
 
   %% Try to grab the data
   fprintf('Downloading Fred Data...');
-  [query, success] = ReadFredData(1, 'series_id', series, 'realtime_start', '1776-07-04', 'realtime_end', '9999-12-31', varargin{:});
+  [query, success] = fred.ReadFredData(1, 'series_id', series, 'realtime_start', '1776-07-04', 'realtime_end', '9999-12-31', varargin{:});
 
   %% Return on errors
   if ~success
@@ -36,19 +36,60 @@ function [vintdata] = vintall(series, varargin)
     end
   end
 
-  %% Loop over unique observation dates and fill in the data matrix
-  vintdata.date           = unq.date;
-  vintdata.realtime_start = unq.realtime_start;
-  Nobs  = length(vintdata.date);
-  Nvint = length(vintdata.realtime_start);
-  vintdata.value = nan(Nobs, Nvint);
-  for n = 1:Nobs
-    match = find(unq.date(n) == all.date);
-    for t = 1:length(match)
-      fillcols = find(all.realtime_start(match(t)) <= unq.realtime_start);
-      vintdata.value(n,fillcols) = all.value(match(t));
+  %% Define a field for all realtime dates
+
+  realtimes = unique([unq.realtime_start; unq.realtime_end]);
+    % Note: Used to fill the data matrix by looping over observations
+    % (looping over rows) and filling the data from realtime start to
+    % realtime start (filling left to right, overwriting vintage dates)
+    % in the vintage data matrix.
+    %
+    % This was maybe problematic. In particular, if there are two
+    % realtime starts (01-Jan-2000 and 01-Jan-2010) with two separate
+    % values, 10 and 20, we used to put a value of 10 from 01-Jan-2000
+    % until 31-Dec-2009. Then 20 from there. That's maybe wrong if
+    % there's a realtime_end for value 10 of, say, 01-Jan-2005.
+    %
+    % This could happen if a data provider decides to drop old
+    % observations because maybe they were wrong for some reason. To be
+    % fair, I have no concrete evidence this has occurred (except some
+    % vague recollection of seeing something like this at some point).
+    % Regardless, the new method implemented below is super safe and
+    % guaranteed to be correct, while the old method was not.
+
+  %% Loop over realtime dates and fill in what you have at that time
+
+    % Set up vintdata structure that will hold everything
+    vintdata.date     = unq.date;
+    vintdata.realtime = realtimes;
+    Nobs  = length(vintdata.date);
+    Nvint = length(vintdata.realtimes);
+    vintdata.value = nan(Nobs, Nvint);
+
+    % Loop over vintdates
+    date2row   = arrayfun(@(dt) find(dt == vintdata.date), all.date); % Matches all dates to their row index in vintdata.date
+    deleteCol  = zeros(1,Nvint);
+    match_last = [];
+    for n = 1:Nvint
+
+      % Indices within "all" that are relevant for this vintage date
+      match = find((realtimes(n) >= all.realtime_start) .* (realtimes(n) <= all.realtime_end));
+
+      % If match for this vintage data is exactly the same as the last,
+      % mark column for delection and move on
+      if length(match) == length(match_last) && ~any(match ~= match_last)
+        deleteCol(n) = 1;
+
+      else % If genuinely different, store the data
+        fillrows = date2row(match);
+        vintdata.value(fillrows,n) = all.value(match);
+        match_last = match;
+      end
     end
-  end
+
+    % Kill the columns that are identical to others
+    vintdata.realtime(find(deleteCol)) = [];
+    vintdata.value(:,find(deleteCol))  = [];
 
 
 end
