a8bb2d714d4f43ec59df0f8c3f6803b4201a8185
Matthew Cocci <Matthew.Cocci@ny.frb.org>
2015-07-02 17:55:32 -0400

    Add max_attempt functionality to getvints

--- +fred/getvints.m
+++ +fred/getvints.m
@@ -1,12 +1,23 @@
-function [vints, success] = getvints(series)
+function [vints, success] = getvints(series, max_attempt)
+
+  if ~exist('max_attempt', 'var')
+    opt = fred.GlobalOptions();
+    max_attempt = opt.max_attempt;
+  end
 
   try
     vints = fred.dispatch_(0, 0, @fred.getvints_, series);
     success = 1;
   catch
-    vints.series = series;
-    vints.vintdates = [];
-    success = 0;
+    if max_attempt - 1
+      pause(3);
+      [vints, success] = getvints(series, max_attempt-1);
+      return
+    else
+      vints.series = series;
+      vints.vintdates = [];
+      success = 0;
+    end
   end
 
 end
