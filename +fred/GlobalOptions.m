4f49bb65733ce95a1f223d5ab8cd09946ce14587
Matthew Cocci <matthew.cocci@gmail.com>
2015-08-12 21:00:25 -0400

    In global options, change {first,last}_realtime to realtime_{start,end}

--- +fred/GlobalOptions.m
+++ +fred/GlobalOptions.m
@@ -1,8 +1,8 @@
 function [opt] = GlobalOptions()
 
   opt.max_attempt = 3;
-  opt.first_realtime = '1776-07-04';
-  opt.last_realtime  = '9999-12-31';
+  opt.realtime_start = '1776-07-04'; % Very first available
+  opt.realtime_end   = '9999-12-31'; % Very last available
 
   % Whether leading and trailing NaNs on a data matrix should be trimmed
   % when possible
